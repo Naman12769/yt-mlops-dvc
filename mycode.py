5dc5dd01129e098cde54cce73491b6e918cb4da1
Naman12769 <namanjainjain341@gmail.com>
2025-05-16 13:24:51 +0530

    second version of data saved

--- mycode.py
+++ mycode.py
@@ -7,11 +7,11 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
     }
 
 df = pd.DataFrame(data)
-new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+new_row_loc = {'Name': 'GF3', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
-new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
-df.loc[len(df.index)] = new_row_loc2
+# new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+# df.loc[len(df.index)] = new_row_loc2
 
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
